52313dc6da2779eaaef64decf7321d4011df871b
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-04 05:36:02 +0000

    [Update]バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  
+  validates :name, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
 
   has_one_attached :profile_image
 
